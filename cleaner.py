ad0ddb62d91890b5d8b1b888a8963cf13cf72b74
btf00005 <btf00005@mix.wvu.edu>
2025-04-27 16:09:13 -0400

    finished the data cleaner
    :)

--- cleaner.py
+++ cleaner.py
@@ -22,7 +22,6 @@ def _():
 def _(pl):
     exchange_rates = pl.read_csv("data/Exchange_Rates.csv")
     exchange_rates
-
     return (exchange_rates,)
 
 
@@ -36,7 +35,7 @@ def _(exchange_rates, pl):
 @app.cell
 def _(exchange, pl):
     rates = exchange.with_columns(pl.col("date").str.to_date("%m/%d/%Y") )
-    rates  
+    rates
     return (rates,)
 
 
@@ -45,6 +44,12 @@ def _(pl, rates):
     final_EXrate = rates.with_columns(pl.col("currency").str.strip_chars() ) ##finished
     final_EXrate
     return (final_EXrate,)
+
+
+@app.cell
+def _(final_EXrate):
+    final_EXrate.write_parquet("data/cleansed/exchange_rates.parquet")
+    return
 
 
 @app.cell
@@ -79,6 +84,12 @@ def _(pl, prod):
 
 
 @app.cell
+def _(final_product):
+    final_product.write_parquet("data/cleansed/product.parquet")
+    return
+
+
+@app.cell
 def _(pl):
     sales = pl.read_csv("data/Sales.csv")
     sales
@@ -108,6 +119,12 @@ def _(money, pl):
 
 
 @app.cell
+def _(final_sales):
+    final_sales.write_parquet("data/cleansed/sales.parquet")
+    return
+
+
+@app.cell
 def _(pl):
     stores = pl.read_csv("data/Stores.csv")
     stores
@@ -133,13 +150,77 @@ def _(pl, sto):
 def _(pl, res):
     final_stores = res.with_columns(pl.col("storekey" , "country" , "state").str.strip_chars() )
     final_stores
+
     return (final_stores,)
+
+
+@app.cell
+def _(final_stores):
+    final_stores.write_parquet("data/cleansed/stores.parquet")
+    return
 
 
 @app.cell
 def _(pl):
     customers = pl.read_csv("data/Customers.csv", schema_overrides={"Zip Code": pl.String}, encoding='iso-8859-1')
+    customers
     return (customers,)
+
+
+@app.cell
+def _(customers, pl):
+    cust = customers.select(pl.all().name.to_lowercase() )
+    cust
+    return (cust,)
+
+
+@app.cell
+def _(cust, pl):
+    omers = cust.with_columns(pl.col("customerkey").cast(pl.String) ,
+                             pl.col("birthday").str.to_date("%m/%d/%Y") )
+    omers
+    return (omers,)
+
+
+@app.cell
+def _(omers, pl):
+    final_customers = omers.with_columns(pl.col("customerkey" , "gender" , "name" , "city" , "state code" , "state" , 
+                                               "zip code" , "country" , "continent").str.strip_chars() )
+    final_customers
+    
+    return (final_customers,)
+
+
+@app.cell
+def _(final_customers):
+    final_customers.write_parquet("data/cleansed/customers.parquet")
+    return
+
+
+@app.cell
+def _(pl):
+    d_d = pl.read_csv("data/Data_Dictionary.csv")
+    d_d
+    return (d_d,)
+
+
+@app.cell
+def _(d_d, pl):
+    data_dick = d_d.select(pl.all().name.to_lowercase())
+    return (data_dick,)
+
+
+@app.cell
+def _(data_dick, pl):
+    d_for_d = data_dick.with_columns(pl.all().str.strip_chars())
+    d_for_d
+    return (d_for_d,)
+
+
+@app.cell
+def _(d_for_d):
+    d_for_d.write_parquet("data/cleansed/data_dictionary.parquet")
+    return
 
 
 if __name__ == "__main__":
